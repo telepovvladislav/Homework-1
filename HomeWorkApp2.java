1e99d58200944bbfcdde54165da8de7d39a8da9a
telepovvladislav <90062387+telepovvladislav@users.noreply.github.com>
2022-03-28 17:08:41 +0300

    Update HomeWorkApp2.java

--- HomeWorkApp2.java
+++ HomeWorkApp2.java
@@ -1,38 +1,39 @@
 /**
  * Java. Lesson 2. Homework 2
  */
+
 public class HomeWorkApp2 {
     public static void main(String[] args) {
         System.out.println(winthin10and20(-20,10));
         isPositiveOrNegative(0);
-        System.out.println(isNegative(-5)); //true, если число отрицательное, false - если положительное.
-        printWorldNTimes("Boom",2);
-        System.out.println(checkYear (2022));
+        System.out.println(isNegative(-5)); //true, если число отрицательное, и false - если положительное.
+        printWorldNTimes("Bay World!",2);
+        System.out.println(checkYear (1984));
     }
 
-    /** Пункт 1.
+    /** Пункт 1
      */
      static boolean winthin10and20 (int x1, int x2) {
         int sum = x1 + x2;
         return (10 <= sum && sum <= 20)? true : false;
     }
 
-    /** Пункт 2.
+    /** Пункт 2
      */
      static void isPositiveOrNegative (int x) {
         System.out.println((x >= 0)? "Positive" : "Negative");
     }
 
-    /** Пункт 3.
+    /** Пункт 3
      */
      static boolean isNegative (int x) {
         return ( x < 0 )? true : false;
     }
 
-    /** Пункт 4.
+    /** Пункт 4
      */
      static void printWorldNTimes (String world, int times) {
         for (int i = 0; i < times; i++) {
-            System.out.println(boom);
+            System.out.println(world);
     }
 }
